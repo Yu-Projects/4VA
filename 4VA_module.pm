2918e568f8fa5e955885c493af88b9f2fbec9d84
Kevin Li Yu <klyu@vt.edu>
2017-10-09 23:34:03 -0400

    split times into two

--- 4VA_module.pm
+++ 4VA_module.pm
@@ -78,20 +78,35 @@ module sites
 	s1: [0..4] init 0;			//state of the site (0 unknown, 1 fire, 2 human, 3 fire+human, 4 nuteral)
 	s2: [0..4] init 0; 			//state of the site (0 unknown, 1 fire, 2 human, 3 fire+human, 4 nuteral)
 
-	[time] s1=0 & (h=1 | a=1 | g=1) -> 0.25:(s1'=1) + 0.25:(s1'=2) + 0.25:(s1'=3) + 0.25:(s1'=4);
-	[time] s1=1 & h=1 & g=1 -> 0.5:(s1'=1) + 0.5:(s1'=4);
-	[time] s1=2 & h=1 -> 0.5:(s1'=2) + 0.5:(s1'=4);
-	[time] s1=3 & h=1 & g=1 -> 0.25:(s1'=1) + 0.25:(s1'=2) + 0.25:(s1'=3) + 0.25:(s1'=4);
-	[time] s1=3 & h=1 -> 0.5:(s1'=1) + 0.5:(s1'=3);
+	[time1] s1=0 & (h=1 | a=1 | g=1) -> 0.25:(s1'=1) + 0.25:(s1'=2) + 0.25:(s1'=3) + 0.25:(s1'=4);
+	[time1] s1=1 & h=1 & g=1 -> 0.5:(s1'=1) + 0.5:(s1'=4);
+	[time1] s1=2 & h=1 -> 0.5:(s1'=2) + 0.5:(s1'=4);
+	[time1] s1=3 & h=1 & g=1 -> 0.25:(s1'=1) + 0.25:(s1'=2) + 0.25:(s1'=3) + 0.25:(s1'=4);
+	[time1] s1=3 & h=1 -> 0.5:(s1'=1) + 0.5:(s1'=3);
 
-	[time] s2=0 & (h=2 | a=2 | g=2) -> 0.25:(s2'=1) + 0.25:(s2'=2) + 0.25:(s2'=3) + 0.25:(s2'=4);
-	[time] s2=1 & h=2 & g=2 -> 0.5:(s2'=1) + 0.5:(s2'=4);
-	[time] s2=2 & h=2 -> 0.5:(s2'=2) + 0.5:(s2'=4);
-	[time] s2=3 & h=2 & g=2 -> 0.25:(s2'=1) + 0.25:(s2'=2) + 0.25:(s2'=3) + 0.25:(s2'=4);
-	[time] s2=3 & h=2 -> 0.5:(s2'=1) + 0.5:(s2'=3);
+	[time2] s2=0 & (h=2 | a=2 | g=2) -> 0.25:(s2'=1) + 0.25:(s2'=2) + 0.25:(s2'=3) + 0.25:(s2'=4);
+	[time2] s2=1 & h=2 & g=2 -> 0.5:(s2'=1) + 0.5:(s2'=4);
+	[time2] s2=2 & h=2 -> 0.5:(s2'=2) + 0.5:(s2'=4);
+	[time2] s2=3 & h=2 & g=2 -> 0.25:(s2'=1) + 0.25:(s2'=2) + 0.25:(s2'=3) + 0.25:(s2'=4);
+	[time2] s2=3 & h=2 -> 0.5:(s2'=1) + 0.5:(s2'=3);
 
-	[] s1=4 | s2=4 -> true; 		// self-loop
+	// recycling loop for keeping times the same
+	[time1] s1=0 -> true;
+	[time1] s1=1 -> true;
+	[time1] s1=2 -> true;
+	[time1] s1=3 -> true;
+	[time1] s1=4 -> true;
+
+	[time2] s2=0 -> true;
+	[time2] s2=1 -> true;
+	[time2] s2=2 -> true;
+	[time2] s2=3 -> true;
+	[time2] s2=4 -> true;
+
+	// finishing loop if site is nuteral
+	//[] s1=4 | s2=4 -> true; 		// self-loop
 	
+	//call for agents
 	[s1_hg] s1 = 1 | s1 = 3 -> true;	//call for agent human/UGV
 	[s1_h] s1 = 2 | s1 = 3 -> true;		//call for agent human
 	[s2_hg] s2 = 1 | s2 = 3 -> true;	//call for agent human/UGV
@@ -99,8 +114,11 @@ module sites
 
 endmodule 
 
-rewards "total_time"
-	[time] true: 1;
+rewards "time1"
+	[time1] true: 1;
+endrewards
+rewards "time2"
+	[time2] true: 1;
 endrewards
 
 //rewards "total_movement"
