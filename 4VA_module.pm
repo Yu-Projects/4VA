feb579d43d8a2aa2b7c2ab45476a18e5d1ca6699
Kevin Li Yu <klyu@vt.edu>
2017-10-10 15:07:42 -0400

    have everything working correctly now/need to clean up agent movements

--- 4VA_module.pm
+++ 4VA_module.pm
@@ -6,124 +6,126 @@ mdp
 //NOTE: when simulating you should only click an agents path once per time step. This is due to the fact you will be doing redundant steps if you click an agents movement multiple times in one time step
 
 const int num_sites=2;
+global agent : [0..1] init 0;
+global s1: [0..4] init 0;			//state of the site (0 unknown, 1 fire, 2 human, 3 fire+human, 4 nuteral)
+global s2: [0..4] init 0; 			//state of the site (0 unknown, 1 fire, 2 human, 3 fire+human, 4 nuteral)
+
 //time=0;
 
 module Human
 	h: [0..num_sites] init 0; // human position
 
-	[] h=0 -> (h'=0);
-	[] h=0 -> (h'=1);
-	[s1_hg] h=0 -> (h'=1);
-	[s1_h] h=0 -> (h'=1);
-	[] h=0 -> (h'=2);
-	[s2_hg] h=0 -> (h'=2);
-	[s2_h] h=0 -> (h'=2);
+	[] agent=0 & h=0 -> (h'=0);
+	[] agent=0 & h=0 -> (h'=1);
+	[s1_hg] agent=0 & h=0 -> (h'=1);
+	[s1_h] agent=0 & h=0 -> (h'=1);
+	[] agent=0 & h=0 -> (h'=2);
+	[s2_hg] agent=0 & h=0 -> (h'=2);
+	[s2_h] agent=0 & h=0 -> (h'=2);
 
-	[] h=1 -> (h'=0);
-	[] h=1 -> (h'=1);
-	[s1_hg] h=1 -> (h'=1);
-	[s1_h] h=1 -> (h'=1);
-	[] h=1 -> (h'=2);
-	[s2_hg] h=1 -> (h'=2);
-	[s2_h] h=1 -> (h'=2);
+	[] agent=0 & h=1 -> (h'=0);
+	[] agent=0 & h=1 -> (h'=1);
+	[s1_hg] agent=0 & h=1 -> (h'=1);
+	[s1_h] agent=0 & h=1 -> (h'=1);
+	[] agent=0 & h=1 -> (h'=2);
+	[s2_hg] agent=0 & h=1 -> (h'=2);
+	[s2_h] agent=0 & h=1 -> (h'=2);
 
-	[] h=2 -> (h'=0);
-	[] h=2 -> (h'=1);
-	[s1_hg] h=2 -> (h'=1);
-	[s1_h] h=2 -> (h'=1);
-	[] h=2 -> (h'=2);
-	[s2_hg] h=2 -> (h'=2);
-	[s2_h] h=2 -> (h'=2);
+	[] agent=0 & h=2 -> (h'=0);
+	[] agent=0 & h=2 -> (h'=1);
+	[s1_hg] agent=0 & h=2 -> (h'=1);
+	[s1_h] agent=0 & h=2 -> (h'=1);
+	[] agent=0 & h=2 -> (h'=2);
+	[s2_hg] agent=0 & h=2 -> (h'=2);
+	[s2_h] agent=0 & h=2 -> (h'=2);
 endmodule
 
 module UGV
 	g: [0..num_sites] init 0; // ugv position
 
-	[] g=0 -> (g'=0);
-	[] g=0 -> (g'=1);
-	[s1_hg] g=0 -> (g'=1);
-	[] g=0 -> (g'=2);
-	[s2_hg] g=0 -> (g'=2);
+	[] agent=0 & g=0 -> (g'=0);
+	[] agent=0 & g=0 -> (g'=1);
+	[s1_hg] agent=0 & g=0 -> (g'=1);
+	[] agent=0 & g=0 -> (g'=2);
+	[s2_hg] agent=0 & g=0 -> (g'=2);
 
-	[] g=1 -> (g'=0);
-	[] g=1 -> (g'=1);
-	[s1_hg] g=1 -> (g'=1);
-	[] g=1 -> (g'=2);
-	[s2_hg] g=1 -> (g'=2);
+	[] agent=0 & g=1 -> (g'=0);
+	[] agent=0 & g=1 -> (g'=1);
+	[s1_hg] agent=0 & g=1 -> (g'=1);
+	[] agent=0 & g=1 -> (g'=2);
+	[s2_hg] agent=0 & g=1 -> (g'=2);
 
-	[] g=2 -> (g'=0);
-	[] g=2 -> (g'=1);
-	[s1_hg] g=2 -> (g'=1);
-	[] g=2 -> (g'=2);
-	[s2_hg] g=2 -> (g'=2);
+	[] agent=0 & g=2 -> (g'=0);
+	[] agent=0 & g=2 -> (g'=1);
+	[s1_hg] agent=0 & g=2 -> (g'=1);
+	[] agent=0 & g=2 -> (g'=2);
+	[s2_hg] agent=0 & g=2 -> (g'=2);
 endmodule
 
 module UAV
 	a: [0..num_sites] init 0; // uav position
 
-	[] a=0 -> (a'=0);
-	[] a=0 -> (a'=1);
-	[] a=0 -> (a'=2);
+	[] agent=0 & a=0 -> (a'=0);
+	[] agent=0 & a=0 -> (a'=1);
+	[] agent=0 & a=0 -> (a'=2);
 
-	[] a=1 -> (a'=0);
-	[] a=1 -> (a'=1);
-	[] a=1 -> (a'=2);
+	[] agent=0 & a=1 -> (a'=0);
+	[] agent=0 & a=1 -> (a'=1);
+	[] agent=0 & a=1 -> (a'=2);
 
-	[] a=2 -> (a'=0);
-	[] a=2 -> (a'=1);
-	[] a=2 -> (a'=2);
+	[] agent=0 & a=2 -> (a'=0);
+	[] agent=0 & a=2 -> (a'=1);
+	[] agent=0 & a=2 -> (a'=2);
 endmodule
 
 module sites
-	s1: [0..4] init 0;			//state of the site (0 unknown, 1 fire, 2 human, 3 fire+human, 4 nuteral)
-	s2: [0..4] init 0; 			//state of the site (0 unknown, 1 fire, 2 human, 3 fire+human, 4 nuteral)
+	s1t: [0..1] init 0;
+	s2t: [0..1] init 0;
+	time: [0..100] init 0;	//reward variable for keeping track of time
 
-	[time1] s1=0 & (h=1 | a=1 | g=1) -> 0.25:(s1'=1) + 0.25:(s1'=2) + 0.25:(s1'=3) + 0.25:(s1'=4);
-	[time1] s1=1 & h=1 & g=1 -> 0.5:(s1'=1) + 0.5:(s1'=4);
-	[time1] s1=2 & h=1 -> 0.5:(s1'=2) + 0.5:(s1'=4);
-	[time1] s1=3 & h=1 & g=1 -> 0.25:(s1'=1) + 0.25:(s1'=2) + 0.25:(s1'=3) + 0.25:(s1'=4);
-	[time1] s1=3 & h=1 -> 0.5:(s1'=1) + 0.5:(s1'=3);
-
-	[time2] s2=0 & (h=2 | a=2 | g=2) -> 0.25:(s2'=1) + 0.25:(s2'=2) + 0.25:(s2'=3) + 0.25:(s2'=4);
-	[time2] s2=1 & h=2 & g=2 -> 0.5:(s2'=1) + 0.5:(s2'=4);
-	[time2] s2=2 & h=2 -> 0.5:(s2'=2) + 0.5:(s2'=4);
-	[time2] s2=3 & h=2 & g=2 -> 0.25:(s2'=1) + 0.25:(s2'=2) + 0.25:(s2'=3) + 0.25:(s2'=4);
-	[time2] s2=3 & h=2 -> 0.5:(s2'=1) + 0.5:(s2'=3);
+	//site1
+	[] s1t=0 & s1=0 & (h=1 | a=1 | g=1) -> 0.25:(s1'=1) & (s1t'=1) & (agent'=1) + 0.25:(s1'=2) & (s1t'=1) & (agent'=1) + 0.25:(s1'=3) & (s1t'=1) & (agent'=1) + 0.25:(s1'=4) & (s1t'=1) & (agent'=1);
+	[] s1t=0 & s1=1 & h=1 & g=1 -> 0.5:(s1'=1) & (s1t'=1) & (agent'=1) + 0.5:(s1'=4) & (s1t'=1) & (agent'=1);
+	[] s1t=0 & s1=2 & h=1 -> 0.5:(s1'=2) & (s1t'=1) & (agent'=1) + 0.5:(s1'=4) & (s1t'=1) & (agent'=1);
+	[] s1t=0 & s1=3 & h=1 & g=1 -> 0.25:(s1'=1) & (s1t'=1) & (agent'=1) + 0.25:(s1'=2) & (s1t'=1) & (agent'=1) + 0.25:(s1'=3) & (s1t'=1) & (agent'=1) + 0.25:(s1'=4) & (s1t'=1) & (agent'=1);
+	[] s1t=0 & s1=3 & h=1 -> 0.5:(s1'=1) & (s1t'=1) & (agent'=1) + 0.5:(s1'=3) & (s1t'=1) & (agent'=1);
+	//site2
+	[] s2t=0 & s2=0 & (h=2 | a=2 | g=2) -> 0.25:(s2'=1) & (s2t'=1) & (agent'=1) + 0.25:(s2'=2) & (s2t'=1) & (agent'=1) + 0.25:(s2'=3) & (s2t'=1) & (agent'=1) + 0.25:(s2'=4) & (s2t'=1) & (agent'=1);
+	[] s2t=0 & s2=1 & h=2 & g=2 -> 0.5:(s2'=1) & (s2t'=1) & (agent'=1) + 0.5:(s2'=4) & (s2t'=1) & (agent'=1);
+	[] s2t=0 & s2=2 & h=2 -> 0.5:(s2'=2) & (s2t'=1) & (agent'=1) + 0.5:(s2'=4) & (s2t'=1) & (agent'=1);
+	[] s2t=0 & s2=3 & h=2 & g=2 -> 0.25:(s2'=1) & (s2t'=1) & (agent'=1) + 0.25:(s2'=2) & (s2t'=1) & (agent'=1) + 0.25:(s2'=3) & (s2t'=1) & (agent'=1) + 0.25:(s2'=4) & (s2t'=1) & (agent'=1);
+	[] s2t=0 & s2=3 & h=2 -> 0.5:(s2'=1) & (s2t'=1) & (agent'=1) + 0.5:(s2'=3) & (s2t'=1) & (agent'=1);
+	
+	//syncer
+	[] s1t=1 & s2t=1 & agent=1 -> 1:(s1t'=0) & (s2t'=0) & (agent'=0) & (time'=time+1);
 
 	// recycling loop for keeping times the same
-	[time1] s1=0 -> true;
-	[time1] s1=1 -> true;
-	[time1] s1=2 -> true;
-	[time1] s1=3 -> true;
-	[time1] s1=4 -> true;
+	[] s1t=0 & s1=0 -> (s1'=0) & (s1t'=1) & (agent'=1);
+	[] s1t=0 & s1=1 -> (s1'=1) & (s1t'=1) & (agent'=1);
+	[] s1t=0 & s1=2 -> (s1'=2) & (s1t'=1) & (agent'=1);
+	[] s1t=0 & s1=3 -> (s1'=3) & (s1t'=1) & (agent'=1);
+	[] s1t=0 & s1=4 -> (s1'=4) & (s1t'=1) & (agent'=1);
 
-	[time2] s2=0 -> true;
-	[time2] s2=1 -> true;
-	[time2] s2=2 -> true;
-	[time2] s2=3 -> true;
-	[time2] s2=4 -> true;
+	[] s2t=0 & s2=0 -> (s2'=0) & (s2t'=1) & (agent'=1);
+	[] s2t=0 & s2=1 -> (s2'=1) & (s2t'=1) & (agent'=1);
+	[] s2t=0 & s2=2 -> (s2'=2) & (s2t'=1) & (agent'=1);
+	[] s2t=0 & s2=3 -> (s2'=3) & (s2t'=1) & (agent'=1);
+	[] s2t=0 & s2=4 -> (s2'=4) & (s2t'=1) & (agent'=1);
 
 	// finishing loop if site is nuteral
 	//[] s1=4 | s2=4 -> true; 		// self-loop
 	
 	//call for agents
-	[s1_hg] s1 = 1 | s1 = 3 -> true;	//call for agent human/UGV
-	[s1_h] s1 = 2 | s1 = 3 -> true;		//call for agent human
-	[s2_hg] s2 = 1 | s2 = 3 -> true;	//call for agent human/UGV
-	[s2_h] s2 = 2 | s2 = 3 -> true;		//call for agent human
+	[s1_hg] agent=0 & s1 = 1 | s1 = 3 -> true;	//call for agent human/UGV
+	[s1_h] agent=0 & s1 = 2 | s1 = 3 -> true;		//call for agent human
+	
+	[s2_hg] agent=0 & s2 = 1 | s2 = 3 -> true;	//call for agent human/UGV
+	[s2_h] agent=0 & s2 = 2 | s2 = 3 -> true;		//call for agent human
 
 endmodule 
 
-rewards "time1"
-	[time1] true: 1;
+
+rewards "total_time"
+	s1t=1 & s2t=1 & agent=1 : 1;
 endrewards
-rewards "time2"
-	[time2] true: 1;
-endrewards
-
-//rewards "total_movement"
-//	true:1;
-//endrewards
-
-
 
--- 4VA_test.pm
+++ 4VA_test.pm
@@ -1,82 +1,138 @@
 mdp
 
-//operation parameters
-//const double p=0.5; // probability of increasing workload due to other uncertain tasks
-//const double accu_load1; // accuracy at the low workload level (real numbers between 0 and 1)
-//const double accu_load2; // accuracy at the high workload level (real numbers between 0 and 1)
-//const double fd; // accuracy discount due to fatigue (real numbers between 0 and 1)
-//const int COUNTER; // fatigue threshold (integers, e.g, 10)
-//const double risky2; // at w2: probability of choosing a risky route
-//const double risky6; // at w6: probability of choosing a risky route
+//TODO: try controller synthesis to optimize properties (including pareto properties)
+//TODO: scale up the number of humans, robots, sites
+//TODO: how do you allow for sites to do both change at the same time without doing an exhaustive creation of lines
+//NOTE: when simulating you should only click an agents path once per time step. This is due to the fact you will be doing redundant steps if you click an agents movement multiple times in one time step
+
+const int num_sites=2;
+//time=0;
 
 
-//formula rewards = //QUESTIONS ON HOW TO INPUT THE REWARD FUNCTION
+module sites
+	s1: [0..4] init 0;			//state of the site (0 unknown, 1 fire, 2 human, 3 fire+human, 4 nuteral)
+	s2: [0..4] init 0; 			//state of the site (0 unknown, 1 fire, 2 human, 3 fire+human, 4 nuteral)
+	h: [0..num_sites] init 0; // human position
+	g: [0..num_sites] init 0; // ugv position
+	a: [0..num_sites] init 0; // uav position
 
-module fire_rescue_single_site
+	s1t: [0..1] init 0;
+	s2t: [0..1] init 0;
+	agent: [0..1] init 0;
+
+	[time1] s1t=0 & s1=0 & (h=1 | a=1 | g=1) -> 0.25:(s1'=1) & (s1t'=1) & (agent'=1) + 0.25:(s1'=2) & (s1t'=1) & (agent'=1) + 0.25:(s1'=3) & (s1t'=1) & (agent'=1) + 0.25:(s1'=4) & (s1t'=1) & (agent'=1);
+	[time1] s1t=0 & s1=1 & h=1 & g=1 -> 0.5:(s1'=1) & (s1t'=1) & (agent'=1) + 0.5:(s1'=4) & (s1t'=1) & (agent'=1);
+	[time1] s1t=0 & s1=2 & h=1 -> 0.5:(s1'=2) & (s1t'=1) & (agent'=1) + 0.5:(s1'=4) & (s1t'=1) & (agent'=1);
+	[time1] s1t=0 & s1=3 & h=1 & g=1 -> 0.25:(s1'=1) & (s1t'=1) & (agent'=1) + 0.25:(s1'=2) & (s1t'=1) & (agent'=1) + 0.25:(s1'=3) & (s1t'=1) & (agent'=1) + 0.25:(s1'=4) & (s1t'=1) & (agent'=1);
+	[time1] s1t=0 & s1=3 & h=1 -> 0.5:(s1'=1) & (s1t'=1) & (agent'=1) + 0.5:(s1'=3) & (s1t'=1) & (agent'=1);
+
+	[time2] s2t=0 & s2=0 & (h=2 | a=2 | g=2) -> 0.25:(s2'=1) & (s2t'=1) & (agent'=1) + 0.25:(s2'=2) & (s2t'=1) & (agent'=1) + 0.25:(s2'=3) & (s2t'=1) & (agent'=1) + 0.25:(s2'=4) & (s2t'=1) & (agent'=1);
+	[time2] s2t=0 & s2=1 & h=2 & g=2 -> 0.5:(s2'=1) & (s2t'=1) & (agent'=1) + 0.5:(s2'=4) & (s2t'=1) & (agent'=1);
+	[time2] s2t=0 & s2=2 & h=2 -> 0.5:(s2'=2) & (s2t'=1) & (agent'=1) + 0.5:(s2'=4) & (s2t'=1) & (agent'=1);
+	[time2] s2t=0 & s2=3 & h=2 & g=2 -> 0.25:(s2'=1) & (s2t'=1) & (agent'=1) + 0.25:(s2'=2) & (s2t'=1) & (agent'=1) + 0.25:(s2'=3) & (s2t'=1) & (agent'=1) + 0.25:(s2'=4) & (s2t'=1) & (agent'=1);
+	[time2] s2t=0 & s2=3 & h=2 -> 0.5:(s2'=1) & (s2t'=1) & (agent'=1) + 0.5:(s2'=3) & (s2t'=1) & (agent'=1);
 	
-	// descriptors
-	s: [0..1] init 0; //sites (site 0 & site 1)
-	d: [0..4] init 0; //state of the site (0 unknown, 1 fire, 2 human, 3 fire+human, 4 nuteral)
-	a: [0..7] init 0; //agents (0 no-one, 1 Human, 2 UGV, 3 UAV, 4 Human+UGV, 5 Human+UAV, 6 UGV+UAV, 7 Human+UGV+UAV)
-	
-	//transitions
-		//moving from unknown (d=0) to a known state (d!=0) with any agent (a!=0)
-		//s=0 has d=1 needs a=4
-		//s=1 has d=2 needs a=1
-//	[] s=0 & d=0 & a=0-> 1/7:(a'=1)&(d'=1)&(s'=0)
-//				+1/7:(a'=2)&(d'=1)&(s'=0)
-//				+1/7:(a'=3)&(d'=1)&(s'=0)
-//				+1/7:(a'=4)&(d'=1)&(s'=0)
-//				+1/7:(a'=5)&(d'=1)&(s'=0)
-//				+1/7:(a'=6)&(d'=1)&(s'=0)
-//				+1/7:(a'=7)&(d'=1)&(s'=0);
-//	[] s=1 & d=0 & a=0-> 1/7:(a'=1)&(d'=2)&(s'=1)
-//				+1/7:(a'=2)&(d'=2)&(s'=1)
-//				+1/7:(a'=3)&(d'=2)&(s'=1)
-//				+1/7:(a'=4)&(d'=2)&(s'=1)
-//				+1/7:(a'=5)&(d'=2)&(s'=1)
-//				+1/7:(a'=6)&(d'=2)&(s'=1)
-//				+1/7:(a'=7)&(d'=2)&(s'=1);
+	//syncer
+	[time]	s1t=1 & s2t=1 & agent=1 -> 1:(s1t'=0) & (s2t'=0) & (agent'=0);
 
-		//from a known site (d!=0) to a nuteral site (d=4) with correct agent (a=1 & d=2 | a=4 & d=1)
-		//these set of equations are for transitions and rewards
-		//they can be changed to accomodate for new state+agent needs/rewards for each transition/probability to enter a state
-//	[] d=1 & a=4 -> 0.8:(a'=4)&(d'=4)&(s'=s) + 0.2:(a'=a)&(d'=d)&(s'=s);
-//	[] d=2 & a=1 -> 0.9:(a'=1)&(d'=4)&(s'=s) + 0.1:(a'=a)&(d'=d)&(s'=s);
-//	[] d=3 & a=4 -> 0.8:(a'=4)&(d'=4)&(s'=s) + 0.1:(a'=a)&(d'=d)&(s'=s) + 0.1:(a'=a)&(d'=1)&(s'=s);
-//	[] d=3 & a=1 -> 0.8:(a'=1)&(d'=1)&(s'=s) + 0.2:(a'=a)&(d'=d)&(s'=s);
-//	[] d=4 -> 1:(d'=4)&(s'=s);
+	// recycling loop for keeping times the same
+	[time1] s1t=0 & s1=0 -> (s1'=0) & (s1t'=1) & (agent'=1);
+	[time1] s1t=0 & s1=1 -> (s1'=1) & (s1t'=1) & (agent'=1);
+	[time1] s1t=0 & s1=2 -> (s1'=2) & (s1t'=1) & (agent'=1);
+	[time1] s1t=0 & s1=3 -> (s1'=3) & (s1t'=1) & (agent'=1);
+	[time1] s1t=0 & s1=4 -> (s1'=4) & (s1t'=1) & (agent'=1);
 
-		//this section is for restricting movement from site to site (s is the main operator)
-//	[] s=0 -> 1:(s'=0);
-//	[] s=1 -> 0.5:(s'=0) + 0.5:(s'=1);	//commented for simplisity and smaller case
+	[time2] s2t=0 & s2=0 -> (s2'=0) & (s2t'=1) & (agent'=1);
+	[time2] s2t=0 & s2=1 -> (s2'=1) & (s2t'=1) & (agent'=1);
+	[time2] s2t=0 & s2=2 -> (s2'=2) & (s2t'=1) & (agent'=1);
+	[time2] s2t=0 & s2=3 -> (s2'=3) & (s2t'=1) & (agent'=1);
+	[time2] s2t=0 & s2=4 -> (s2'=4) & (s2t'=1) & (agent'=1);
 
-		//disaster state change (d is the main operator)
-	[] d=0 & (a!=0)-> 1/4:(d'=1) + 1/4:(d'=2) + 1/4:(d'=3) + 1/4:(d'=4);
-	[] d=1 & (a=4 | a=7) -> 0.5:(d'=1) + 0.5:(d'=4);
-	[] d=2 & (a=1 | a=4 | a=5 | a=7) -> 0.5:(d'=2) + 0.5:(d'=4);
-	[] d=3 & (a=4 | a=7)-> 0.25:(d'=1) + 0.25:(d'=2) + 0.25:(d'=3) + 0.25:(d'=4);
-	[] d=3 & (a=1 | a=5)-> 0.5:(d'=1) + 0.5:(d'=3);
-	[] d=4 -> 1:(d'=4);
-
-		//random agent movement (a is the main operator)
-	[] a=0 & (d=0)-> 1/8:(a'=0) + 1/8:(a'=1) + 1/8:(a'=2) + 1/8:(a'=3) + 1/8:(a'=4) + 1/8:(a'=5) + 1/8:(a'=6) + 1/8:(a'=7);
-	[] a=1 & d!=0 & (d=2 | d=3) -> 1:(a'=1);
-	[] a=1 & d!=0 & (d=1) -> 1/7:(a'=0) + 1/7:(a'=2) + 1/7:(a'=3) + 1/7:(a'=4) + 1/7:(a'=5) + 1/7:(a'=6) + 1/7:(a'=7);
-	[] a=2 & (d!=0) -> 1/7:(a'=0) + 1/7:(a'=1) + 1/7:(a'=3) + 1/7:(a'=4) + 1/7:(a'=5) + 1/7:(a'=6) + 1/7:(a'=7);
-	[] a=3 & (d!=0) -> 1/7:(a'=0) + 1/7:(a'=1) + 1/7:(a'=2) + 1/7:(a'=4) + 1/7:(a'=5) + 1/7:(a'=6) + 1/7:(a'=7);
-	[] a=4 & d!=0 & (d=1 | d=2 | d=3) -> 1:(a'=4);
-//	[] a=4 & d!=0 & (d=4) -> 1/7:(a'=0) + 1/7:(a'=1) + 1/7:(a'=2) + 1/7:(a'=3) + 1/7:(a'=5) + 1/7:(a'=6) + 1/7:(a'=7);
-	[] a=5 & d!=0 & (d=2 | d=3) -> 1:(a'=5);
-	[] a=5 & d!=0 & (d=1) -> 1/7:(a'=0) + 1/7:(a'=1) + 1/7:(a'=2) + 1/7:(a'=3) + 1/7:(a'=4) + 1/7:(a'=6) + 1/7:(a'=7);
-	[] a=6 & (d!=0) -> 1/8:(a'=0) + 1/8:(a'=1) + 1/8:(a'=2) + 1/8:(a'=3) + 1/8:(a'=4) + 1/8:(a'=5) + 1/8:(a'=6) + 1/8:(a'=7);
-	[] a=7 & d!=0 & (d=1 | d=2 | d=3) -> 1:(a'=7);
-//	[] a=7 & d!=0 & (d=4) -> 1/7:(a'=0) + 1/7:(a'=1) + 1/7:(a'=2) + 1/7:(a'=3) + 1/7:(a'=4) + 1/7:(a'=5) + 1/7:(a'=6);
+	// finishing loop if site is nuteral
+	//[] s1=4 | s2=4 -> true; 		// self-loop
 	
 
-endmodule
+	//AGENTS
+	//call for agents
+	[s1_hg] agent=0 & (s1 = 1 | s1 = 3) -> true;	//call for agent human/UGV
+	[s1_h] agent=0 & (s1 = 2 | s1 = 3) -> true;		//call for agent human
+	
+	[s2_hg] agent=0 & (s2 = 1 | s2 = 3) -> true;	//call for agent human/UGV
+	[s2_h] agent=0 & (s2 = 2 | s2 = 3) -> true;		//call for agent human
 
-rewards	"site_reward"
-//	s=0 & d=4 & (a=1: 5;
-	s=0 & d=4 : 10;
+	//HUMAN
+	[] agent=0 & h=0 -> (h'=0);
+	[] agent=0 & h=0 -> (h'=1);
+	[s1_hg] agent=0 & h=0 -> (h'=1);
+	[s1_h] agent=0 & h=0 -> (h'=1);
+	[] agent=0 & h=0 -> (h'=2);
+	[s2_hg] agent=0 & h=0 -> (h'=2);
+	[s2_h] agent=0 & h=0 -> (h'=2);
+
+	[] agent=0 & h=1 -> (h'=0);
+	[] agent=0 & h=1 -> (h'=1);
+	[s1_hg] agent=0 & h=1 -> (h'=1);
+	[s1_h] agent=0 & h=1 -> (h'=1);
+	[] agent=0 & h=1 -> (h'=2);
+	[s2_hg] agent=0 & h=1 -> (h'=2);
+	[s2_h] agent=0 & h=1 -> (h'=2);
+
+	[] agent=0 & h=2 -> (h'=0);
+	[] agent=0 & h=2 -> (h'=1);
+	[s1_hg] agent=0 & h=2 -> (h'=1);
+	[s1_h] agent=0 & h=2 -> (h'=1);
+	[] agent=0 & h=2 -> (h'=2);
+	[s2_hg] agent=0 & h=2 -> (h'=2);
+	[s2_h] agent=0 & h=2 -> (h'=2);
+
+	//UGV
+	[] agent=0 & g=0 -> (g'=0);
+	[] agent=0 & g=0 -> (g'=1);
+	[s1_hg] agent=0 & g=0 -> (g'=1);
+	[] agent=0 & g=0 -> (g'=2);
+	[s2_hg] agent=0 & g=0 -> (g'=2);
+
+	[] agent=0 & g=1 -> (g'=0);
+	[] agent=0 & g=1 -> (g'=1);
+	[s1_hg] agent=0 & g=1 -> (g'=1);
+	[] agent=0 & g=1 -> (g'=2);
+	[s2_hg] agent=0 & g=1 -> (g'=2);
+
+	[] agent=0 & g=2 -> (g'=0);
+	[] agent=0 & g=2 -> (g'=1);
+	[s1_hg] agent=0 & g=2 -> (g'=1);
+	[] agent=0 & g=2 -> (g'=2);
+	[s2_hg] agent=0 & g=2 -> (g'=2);
+
+	//UAV
+	[] agent=0 & a=0 -> (a'=0);
+	[] agent=0 & a=0 -> (a'=1);
+	[] agent=0 & a=0 -> (a'=2);
+
+	[] agent=0 & a=1 -> (a'=0);
+	[] agent=0 & a=1 -> (a'=1);
+	[] agent=0 & a=1 -> (a'=2);
+
+	[] agent=0 & a=2 -> (a'=0);
+	[] agent=0 & a=2 -> (a'=1);
+	[] agent=0 & a=2 -> (a'=2);
+
+endmodule 
+
+rewards "time1"
+	[time1] true: 1;
 endrewards
+rewards "time2"
+	[time2] true: 1;
+endrewards
+rewards "total_time"
+	[time] true: 1;
+endrewards
+
+//rewards "total_movement"
+//	true:1;
+//endrewards
+
+
+
+
