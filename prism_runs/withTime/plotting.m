736faafdfd74e72d3adcf0989a044ac6af7a903e
Kevin Li Yu <klyu@vt.edu>
2017-12-06 12:01:02 -0500

    added stochasticMovement figures and files

---
+++ prism_runs/withTime/plotting.m
@@ -0,0 +1,17 @@
+
+load data.mat;
+figure(1);
+plot(VA(:,1), VA(:,2));
+title('#sites vs. states');
+
+figure(2);
+plot(VA(:,1), VA(:,3))
+title('#sites vs. model construction time')
+
+figure(3);
+plot(VA(:,1), VA(:,4))
+title('#sites vs. model checking time')
+
+figure(4)
+plot(VA(:,1), VA(:,5))
+title('#sites vs. states');
