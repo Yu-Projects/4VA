68f549d14e86ef89e682f5475b29baaa0e1407d3
Kevin Li Yu <klyu@vt.edu>
2017-12-09 13:55:58 -0500

    updated timer_new

--- 4VA_timer_new.pm
+++ 4VA_timer_new.pm
@@ -5,13 +5,40 @@ mdp
 // DISTANCE CONSTANTS
 const int D01 = 2; // distance between site0 and site1
 const int D02 = 3; // distance between site0 and site2
+const int D03 = 4;
+const int D04 = 2;
+const int D05 = 2;
+const int D06 = 3;
+
 const int D12 = 4; // distance between site1 and site2
+const int D13=3;
+const int D14=3;
+const int D15=2;
+const int D16=4;
+
+const int D23=4;
+const int D24=2;
+const int D25=3;
+const int D26=2;
+
+const int D34=2;
+const int D35=3;
+const int D36=4;
+
+const int D45=4;
+const int D46=3;
+
+const int D56=2;
 
 // SITES
 const int N = 2; // number of sites
 const int site0 = 0;
 const int site1 = 1;
 const int site2 = 2;
+const int site3 = 3;
+const int site4 = 4;
+const int site5 = 5;
+const int site6 = 6;
 
 // TIMING CONSTANTS
 const int Ch_MAX = 100; // max vlaue of clock_h
@@ -32,7 +59,7 @@ const int state3 = 3; // fire+human
 const int state4 = 4; // nuteral
 
 // TIMER INFORMATION
-const int totClock_max = 1000;
+const int totClock_max = 50;
 
 module timer
 	totClock: [0..totClock_max] init 0;
@@ -54,22 +81,68 @@ module human
 	// human movement between sites
 	[human_0_1] h=site0 & !move_h -> (h'=site1) & (move_h'=true);
 	[human_0_2] h=site0 & !move_h -> (h'=site2) & (move_h'=true);
+//	[human_0_3] h=site0 & !move_h -> (h'=site3) & (move_h'=true);
+//	[human_0_4] h=site0 & !move_h -> (h'=site4) & (move_h'=true);
+//	[human_0_5] h=site0 & !move_h -> (h'=site5) & (move_h'=true);
+//	[human_0_6] h=site0 & !move_h -> (h'=site6) & (move_h'=true);
 	[human_1_0] h=site1 & !move_h -> (h'=site0) & (move_h'=true);
 	[human_1_2] h=site1 & !move_h -> (h'=site2) & (move_h'=true);
+//	[human_1_3] h=site1 & !move_h -> (h'=site3) & (move_h'=true);
+//	[human_1_4] h=site1 & !move_h -> (h'=site4) & (move_h'=true);
+//	[human_1_5] h=site1 & !move_h -> (h'=site5) & (move_h'=true);
+//	[human_1_6] h=site1 & !move_h -> (h'=site6) & (move_h'=true);
 	[human_2_0] h=site2 & !move_h -> (h'=site0) & (move_h'=true);
 	[human_2_1] h=site2 & !move_h -> (h'=site1) & (move_h'=true);
+//	[human_2_3] h=site2 & !move_h -> (h'=site3) & (move_h'=true);
+//	[human_2_4] h=site2 & !move_h -> (h'=site4) & (move_h'=true);
+//	[human_2_5] h=site2 & !move_h -> (h'=site5) & (move_h'=true);
+//	[human_2_6] h=site2 & !move_h -> (h'=site6) & (move_h'=true);
+//	[human_3_0] h=site3 & !move_h -> (h'=site0) & (move_h'=true);
+//	[human_3_1] h=site3 & !move_h -> (h'=site1) & (move_h'=true);
+//	[human_3_2] h=site3 & !move_h -> (h'=site2) & (move_h'=true);
+//	[human_3_4] h=site3 & !move_h -> (h'=site4) & (move_h'=true);
+//	[human_3_5] h=site3 & !move_h -> (h'=site5) & (move_h'=true);
+//	[human_3_6] h=site3 & !move_h -> (h'=site6) & (move_h'=true);
+//	[human_4_0] h=site4 & !move_h -> (h'=site0) & (move_h'=true);
+//	[human_4_1] h=site4 & !move_h -> (h'=site1) & (move_h'=true);
+//	[human_4_2] h=site4 & !move_h -> (h'=site2) & (move_h'=true);
+//	[human_4_3] h=site4 & !move_h -> (h'=site3) & (move_h'=true);
+//	[human_4_5] h=site4 & !move_h -> (h'=site5) & (move_h'=true);
+//	[human_4_6] h=site4 & !move_h -> (h'=site6) & (move_h'=true);
+//	[human_5_0] h=site5 & !move_h -> (h'=site0) & (move_h'=true);
+//	[human_5_1] h=site5 & !move_h -> (h'=site1) & (move_h'=true);
+//	[human_5_2] h=site5 & !move_h -> (h'=site2) & (move_h'=true);
+//	[human_5_3] h=site5 & !move_h -> (h'=site3) & (move_h'=true);
+//	[human_5_4] h=site5 & !move_h -> (h'=site4) & (move_h'=true);
+//	[human_5_6] h=site5 & !move_h -> (h'=site6) & (move_h'=true);
+//	[human_6_0] h=site6 & !move_h -> (h'=site0) & (move_h'=true);
+//	[human_6_1] h=site6 & !move_h -> (h'=site1) & (move_h'=true);
+//	[human_6_2] h=site6 & !move_h -> (h'=site2) & (move_h'=true);
+//	[human_6_3] h=site6 & !move_h -> (h'=site3) & (move_h'=true);
+//	[human_6_4] h=site6 & !move_h -> (h'=site4) & (move_h'=true);
+//	[human_6_5] h=site6 & !move_h -> (h'=site5) & (move_h'=true);
 endmodule
 
 // uav agent
 module uav = human [h=a, move_h=move_a, Ch_MAX=Ca_MAX, Sh=Sa,
-			human_0_1=uav_0_1, human_0_2=uav_0_2, human_1_0=uav_1_0, human_1_2=uav_1_2,
-			human_2_0=uav_2_0, human_2_1=uav_2_1]
+			human_0_1=uav_0_1, human_0_2=uav_0_2, human_0_3=uav_0_3, human_0_4=uav_0_4, human_0_5=uav_0_5,human_0_6=uav_0_6,
+			human_1_0=uav_1_0, human_1_2=uav_1_2, human_1_3=uav_1_3, human_1_4=uav_1_4, human_1_5=uav_1_5,human_1_6=uav_1_6,
+			human_2_0=uav_2_0, human_2_1=uav_2_1, human_2_3=uav_2_3, human_2_4=uav_2_4, human_2_5=uav_2_5,human_2_6=uav_2_6,
+			human_3_0=uav_3_0, human_3_1=uav_3_1, human_3_2=uav_3_2, human_3_4=uav_3_4, human_3_5=uav_3_5,human_3_6=uav_3_6,
+			human_4_0=uav_4_0, human_4_1=uav_4_1, human_4_2=uav_4_2, human_4_3=uav_4_3, human_4_5=uav_4_5,human_4_6=uav_4_6,
+			human_5_0=uav_5_0, human_5_1=uav_5_1, human_5_2=uav_5_2, human_5_3=uav_5_3, human_5_4=uav_5_4,human_5_6=uav_5_6,
+			human_6_0=uav_6_0, human_6_1=uav_6_1, human_6_2=uav_6_2, human_6_3=uav_6_3, human_6_4=uav_6_4,human_6_5=uav_6_5]
 endmodule
 
 // ugv agent
 module ugv = human [h=g, move_h=move_g, Ch_MAX=Cg_MAX, Sh=Sg,
-			human_0_1=ugv_0_1, human_0_2=ugv_0_2, human_1_0=ugv_1_0, human_1_2=ugv_1_2,
-			human_2_0=ugv_2_0, human_2_1=ugv_2_1]
+			human_0_1=ugv_0_1, human_0_2=ugv_0_2, human_0_3=ugv_0_3, human_0_4=ugv_0_4, human_0_5=ugv_0_5,human_0_6=ugv_0_6,
+			human_1_0=ugv_1_0, human_1_2=ugv_1_2, human_1_3=ugv_1_3, human_1_4=ugv_1_4, human_1_5=ugv_1_5,human_1_6=ugv_1_6,
+			human_2_0=ugv_2_0, human_2_1=ugv_2_1, human_2_3=ugv_2_3, human_2_4=ugv_2_4, human_2_5=ugv_2_5,human_2_6=ugv_2_6,
+			human_3_0=ugv_3_0, human_3_1=ugv_3_1, human_3_2=ugv_3_2, human_3_4=ugv_3_4, human_3_5=ugv_3_5,human_3_6=ugv_3_6,
+			human_4_0=ugv_4_0, human_4_1=ugv_4_1, human_4_2=ugv_4_2, human_4_3=ugv_4_3, human_4_5=ugv_4_5,human_4_6=ugv_4_6,
+			human_5_0=ugv_5_0, human_5_1=ugv_5_1, human_5_2=ugv_5_2, human_5_3=ugv_5_3, human_5_4=ugv_5_4,human_5_6=ugv_5_6,
+			human_6_0=ugv_6_0, human_6_1=ugv_6_1, human_6_2=ugv_6_2, human_6_3=ugv_6_3, human_6_4=ugv_6_4,human_6_5=ugv_6_5]
 endmodule
 
 // main site module
@@ -88,6 +161,10 @@ endmodule
 
 // duplicate site modules
 module site_two = site_one[s1=s2, site1=site2] endmodule
+//module site_three = site_one[s1=s3, site1=site3] endmodule
+//module site_four = site_one[s1=s4, site1=site4] endmodule
+//module site_five = site_one[s1=s5, site1=site5] endmodule
+//module site_six = site_one[s1=s6, site1=site6] endmodule
 
 // counter for time
 rewards "time"
@@ -95,5 +172,5 @@ rewards "time"
 endrewards
 
 // finish state
-label "done" = s1=4 & s2=4; //& s3=4;  & s4=4 & s5=4 & s6=4;
+label "done" = s1=4 & s2=4;// & s3=4;// & s4=4 & s5=4;// & s6=4;
 
